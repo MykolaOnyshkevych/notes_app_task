72c1fa1fa1d4115b8430d2c193864be2ef1f2312
MykolaOnyshkevych <onyshkogod@gmail.com>
2022-08-12 11:40:20 +0300

    added indexeddb to the app

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,10 @@ const filterPosts = (notes, text) => {
 };
 
 function App() {
-    //set the database 
+    // database 
     const db = new Dexie("NotesStorage");
-    //create the database store
     db.version(1).stores({
-        notes: "id, title, content"
+        notesstore: "++id, title, body, lastModified"
     })
   
 
@@ -31,44 +30,56 @@ const [modalIsOpen, setModalIsOpen] = useState(false);
 const [text, setText] = useState(false);
 
 
-  const [notes, setNotes] = useState(
-    localStorage.notes ? JSON.parse(localStorage.notes) : []
-  );
+  const [notes, setNotes] = useState([]);
+
+
+
+
+
+useEffect(() => {
+    const getPosts = async() => {
+        let allNotes = await db.notesstore.toArray();
+        setNotes(allNotes);
+    }
+    getPosts();
+}, [])
+
+
+	const onAddNote = () => {
+        const newNote = {
+          id: uuid(),
+          title: "Untitled Note",
+          body: "",
+          lastModified: Date.now(),
+        };
+          
+        db.notesstore.add(newNote).then(async() => {
+            setNotes([newNote, ...notes]);
+            setActiveNote(newNote.id); 
+        });    
+        
+    }
+
+
    const [activeNote, setActiveNote] = useState(false);
    const filteredPosts = filterPosts(notes, text);
 
-
-  useEffect(() => {
-    localStorage.setItem("notes", JSON.stringify(notes));
-  }, [notes]);
-
-  const onAddNote = () => {
-    const newNote = {
-      id: uuid(),
-      title: "Untitled Note",
-      body: "",
-      lastModified: Date.now(),
-    };
-
-    setNotes([newNote, ...notes]);
-    setActiveNote(newNote.id);
-  };
-
-
-  const onDeleteNote = (noteId) => {
+  const onDeleteNote = async(noteId) => {
     setModalIsOpen(false);
+    db.notesstore.delete(noteId);
     setNotes(notes.filter(({ id }) => id !== noteId));
   };
+
 
   const onUpdateNote = (updatedNote) => {
     const updatedNotesArr = notes.map((note) => {
       if (note.id === updatedNote.id) {
+        db.notesstore.update(note.id, updatedNote);
         return updatedNote;
       }
 
       return note;
     });
-
     setNotes(updatedNotesArr);
   };
 
